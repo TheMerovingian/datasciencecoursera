3b7210941e9fe8349e73e4a3708f6f06ad97f84f
TheMerovingian <bryan.quill@gmail.com>
2015-01-09 19:05:43 +1000

    Just basic package manipulation code

---
+++ Information.R
@@ -0,0 +1,12 @@
+# Package List
+packs <- available.packages()
+
+# Package Installation
+# Multiple packages can be installed using a character array - c("pack1", "pack2")
+install.packages("PackageName")
+
+# Load Package
+library(package)    # No quote around package
+
+# Display current open packages
+search()
